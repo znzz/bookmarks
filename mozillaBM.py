a2f46d3d7ccdba7809a3e656e350a280352fe31f
znzz <znbcode@gmail.com>
2014-06-20 16:00:46 -0400

    Update mozillaBM.py

--- mozillaBM.py
+++ mozillaBM.py
@@ -9,8 +9,8 @@ import sqlite3
 import MySQLdb
 
 
-#retrieves bookmarks from Firefox sqlite3 database
-def getBookmarks(filename):
+"""retrieves bookmarks from Firefox sqlite3 database"""
+def get_mozilla_bm(filename):
     bmlist = []
     db = sqlite3.connect(filename)
     cursor = db.cursor()
@@ -32,6 +32,32 @@ class BookmarksDb(object):
       self.db = db
       self.filename = filename
       self.tablename = tablename
+
+
+    """retrieves all bookmarks already stored in mysql"""
+    def dbBookmarks(self, host, user, password, db, tablename):
+        bmList = []
+        Db = MySQLdb.connect(self.host, self.user, self.password, self.db)
+        cursor1 = Db.cursor()
+        cursor1.execute('SELECT * FROM %s' %(self.tablename))
+        results = cursor1.fetchall()
+        for row in results:
+            bookmark = row[0]
+            bmList.append(bookmark)
+        Db.close()
+        return bmList 
+        
+        
+    #maybe return dict from dbBookmarks, retreival will be faster
+    def send_new_bm(self, filename, host, user, password, db, tablename):
+        new_bm_list = get_mozilla_bm(self.filename)
+        bm_list = self.dbBookmarks(self.host, self.user, self.password, self.db, self.tablename)
+        for newBM in new_bm_list:
+            if not newBM in bm_list:
+                #bm_list.append(newBM)
+                print newBM
+
+
 
 #sends bookmarks to MySQL database using Python MySQLdb api
   def sendBMs(self, filename, tablename):
